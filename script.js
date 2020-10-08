0361bbc4c7bb1bb2591e007f04e0d6fe83c8ee62
Bennett Gould <bgould@protonmail.com>
2020-10-08 03:30:26 -0400

    adding moment.js logic

--- script.js
+++ script.js
@@ -9,11 +9,15 @@ $(function(){
         $('<div>',{
             class: 'row' + ' r' + i
         }).appendTo('.container')
-
-        .append('<div class="col-md-1 time">Time</div>')
-        .append('<div class="col-md-10 content point">content</div>')
+        .append('<div class="col-md-2 time">Time</div>')
+        .append('<div class="col-md-9 content point">content</div>')
         .append('<div class="col-md-1 save point">save</div>');
+        
+        let t = moment();
+        let t2 = moment(t).format('hh:mm:ss a');
+        console.log(t2);
+        $('.time').text(t2 + '');
     }
 
-
 });
+
