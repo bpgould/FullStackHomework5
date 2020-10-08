673945ccff9bb5423ae086c3282b6737dba93b47
Bennett Gould <bgould@protonmail.com>
2020-10-08 02:14:22 -0400

    added buttons and HTML to support event listeners for performing actions on the calendar

--- script.js
+++ script.js
@@ -5,4 +5,15 @@
 //when DOM ready
 $(function(){
 
+    for(i=0;i<12;i++){
+        $('<div>',{
+            class: 'row' + ' r' + i
+        }).appendTo('.container')
+
+        .append('<div class="col-md-1 time">Time</div>')
+        .append('<div class="col-md-10 content point">content</div>')
+        .append('<div class="col-md-1 save point">save</div>');
+    }
+
+
 });
